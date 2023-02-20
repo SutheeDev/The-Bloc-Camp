4d54644f9257938c35c41ac0e68ca551d58a550b
SU-T <guitaristpeet@gmail.com>
2023-02-20 11:00:49 -0800

    add navigation bar to the Landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,12 @@
+import Landing from "./pages/Landing.js";
+
 function App() {
-  return <h1>App</h1>;
+  return (
+    <>
+      <h1>App</h1>
+      <Landing />
+    </>
+  );
 }
 
 export default App;
