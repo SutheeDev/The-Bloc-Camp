5c01270102433c51c5a2bfe06a67fb022ed47e29
SU-T <guitaristpeet@gmail.com>
2023-02-28 15:36:37 -0800

    style Contact page

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -18,16 +18,16 @@ const Contact = () => {
           <div class="map-address">
             <div class="map">
               <div class="square"></div>
-              {/* <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3153.8296652917847!2d-122.38940054869543!3d37.77059197966096!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808f7f1a33f38ea7%3A0xe4941e6c1dc4022b!2s500%20Terry%20A%20Francois%20Blvd%2C%20San%20Francisco%2C%20CA%2094158!5e0!3m2!1sen!2sus!4v1671477213255!5m2!1sen!2sus"
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3153.829665291793!2d-122.38940054869533!3d37.77059197966076!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808f7f1a33f38ea7%3A0xe4941e6c1dc4022b!2s500%20Terry%20A%20Francois%20Blvd%2C%20San%20Francisco%2C%20CA%2094158!5e0!3m2!1sen!2sus!4v1677626702353!5m2!1sen!2sus"
                 width="686"
                 height="514.5"
-                style="border: 0"
+                style={{ border: "0" }}
                 allowfullscreen=""
                 loading="lazy"
                 referrerpolicy="no-referrer-when-downgrade"
-                class="google"
-              ></iframe> */}
+                className="google"
+              ></iframe>
             </div>
             <div class="address-section">
               <h4>Address</h4>
@@ -71,6 +71,93 @@ const Wrapper = styled.main`
     margin: 0 auto;
     @media screen and (max-width: 850px) {
       width: 80vw;
+    }
+  }
+  .contact-info {
+    background-color: #221333;
+    padding: 0 0 20px 0;
+    margin-top: -20px;
+  }
+  .contact-info-container {
+    width: 80vw;
+    margin: 0 auto;
+    font-family: sans-serif;
+  }
+  .map {
+    width: 100%;
+    height: 100%;
+    position: relative;
+    margin-bottom: 40px;
+  }
+  .google {
+    width: 100%;
+    height: 50vh;
+  }
+  .square {
+    display: none;
+  }
+  .contact-info-container h4 {
+    font-size: 24px;
+    font-weight: 300;
+    color: #221333;
+    background-color: #df3311;
+    width: fit-content;
+    letter-spacing: 1px;
+    margin-bottom: 20px;
+  }
+  .address {
+    width: 200px;
+  }
+  .contact-info-container p {
+    font-size: 15px;
+    font-weight: 100;
+    letter-spacing: 1px;
+    margin-bottom: 20px;
+    line-height: 1.9;
+  }
+  .map-address {
+    margin-bottom: 40px;
+  }
+  @media screen and (min-width: 850px) {
+    .contact-info {
+      padding: 40px 0 0 0;
+    }
+    .contact-info-container {
+      width: 85vw;
+      max-width: 1200px;
+      padding: 40px 0;
+    }
+    .square {
+      position: absolute;
+      width: 250px;
+      height: 90px;
+      background-color: #ffffff;
+      top: 0;
+      right: 0;
+      transform: translate(8px, -8px);
+      z-index: 1;
+    }
+    .map-address {
+      display: flex;
+      flex-direction: row-reverse;
+      justify-content: space-between;
+    }
+    .address-section {
+      width: 50%;
+    }
+    .contact-us {
+      display: flex;
+      justify-content: flex-start;
+      align-items: flex-start;
+      gap: calc(15vw + 0.5rem);
+    }
+    .contact-us-info p:first-child {
+      width: 295px;
+    }
+  }
+  @media screen and (min-width: 1150px) {
+    .contact-us {
+      gap: 196px;
     }
   }
 `;
