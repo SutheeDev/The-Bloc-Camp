7c5d071b62ea613cdc01aeb497015a1bca2941aa
SU-T <guitaristpeet@gmail.com>
2023-03-01 10:26:41 -0800

    add initial setup in server.js

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log("Server running...");
+import express from "express";
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello User");
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server running on port : ${port}...`);
+});
