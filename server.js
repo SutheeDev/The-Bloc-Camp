97a295b1f0c595fa5b39c158890a2b86369660c7
SU-T <guitaristpeet@gmail.com>
2023-03-01 11:05:09 -0800

    connect to database

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ dotenv.config();
 import NotFoundMiddleware from "./middleware/not-found.js";
 import ErrorHandlerMiddleware from "./middleware/error-handler.js";
 
+// connect to database function
+import connectDB from "./db/connect.js";
+
 app.get("/", (req, res) => {
   res.send("Hello User");
 });
@@ -17,6 +20,15 @@ app.use(ErrorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-  console.log(`Server running on port : ${port}...`);
-});
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`Server running on port : ${port}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
