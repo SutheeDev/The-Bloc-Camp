00d4d282caf53ad9f64d337fc44eeffcf0636490
SU-T <guitaristpeet@gmail.com>
2023-03-01 16:54:02 -0800

    check missing input and unique email in error-handler

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,6 +1,28 @@
+import { StatusCodes } from "http-status-codes";
+
 const ErrorHandlerMiddleware = (err, req, res, next) => {
   console.log(err);
-  res.status(500).json({ msg: "There was an error" });
+
+  const defaultError = {
+    statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: "Something went wrong",
+  };
+
+  if (err.name === "ValidationError") {
+    defaultError.statusCode = StatusCodes.BAD_REQUEST;
+    defaultError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(", ");
+  }
+
+  if (err.code && err.code === 11000) {
+    defaultError.statusCode = StatusCodes.BAD_REQUEST;
+    defaultError.msg = `This ${Object.keys(
+      err.keyValue
+    )} has already been taken`;
+  }
+
+  res.status(defaultError.statusCode).json({ msg: defaultError.msg });
 };
 
 export default ErrorHandlerMiddleware;
