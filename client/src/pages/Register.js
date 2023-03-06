8bb0c9e472b9a47a66c78244f4ea90c18fc2f45f
SU-T <guitaristpeet@gmail.com>
2023-03-06 10:32:31 -0800

    fix styles of register form

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -17,7 +17,7 @@ const Register = () => {
               type="text"
               placeholder="Name *"
               name="name"
-              class="username"
+              class="name"
             />
             <input
               autocomplete="off"
@@ -86,17 +86,12 @@ const Wrapper = styled.main`
   /* Login Page, Subscribe Page, Subscribed Page */
   /* ------------------- */
   .login {
-    background-color: #221333;
-    /* background-color: blue; */
     padding: 30px 0 30px 0;
-    /* height: 68vh; */
-    /* overflow: hidden; */
   }
   .login-container {
     width: 90vw;
     margin: 0 auto;
     max-width: 450px;
-    /* min-width: 450px; */
   }
   .login h1 {
     text-transform: uppercase;
@@ -105,41 +100,27 @@ const Wrapper = styled.main`
     background-color: transparent;
     padding: 0 0 25px 22px;
     line-height: 0;
+    position: relative;
+    z-index: 1;
   }
   /* ------------------- */
   /* Form */
   /* ------------------- */
-  .thank-message {
-    padding: 40px 20px;
-    justify-content: center;
-    align-items: center;
-  }
-  .thank-message h3 {
-    text-transform: capitalize;
-    font-size: calc(1.5vw + 0.5rem);
-    letter-spacing: 1px;
-    margin-bottom: 40px;
-    line-height: 1.2;
-  }
-  .username,
+  .name,
   .password,
-  .firstname,
-  .lastname,
-  input[type="email"].email-address {
+  .email {
     width: 100%;
     outline: none;
     padding: 1rem 1.5rem 1rem 1rem;
     border: none;
     border-width: 3px;
-    border-color: #213333;
+    border-color: var(--darkBlue);
     border-style: solid;
     margin: 0 0 40px 0;
   }
-  .username::placeholder,
+  .name::placeholder,
   .password::placeholder,
-  .firstname::placeholder,
-  .lastname::placeholder,
-  input[type="email"].email-address::placeholder {
+  .email::placeholder {
     font-size: 14px;
     color: #84c3c3;
   }
@@ -151,23 +132,9 @@ const Wrapper = styled.main`
     width: 142px;
     height: 53px;
   }
-  .subscribed-btn-block {
-    position: relative;
-    width: 250px;
-    height: 53px;
-  }
-  .btn.login-btn {
-    width: 100%;
-    color: #221333;
-    border: 3px solid #221333;
-    position: absolute;
-    top: 0;
-    left: 0;
-    z-index: 1;
-  }
   .btn.login-btn:hover {
-    background-color: #221333;
-    border: 3px solid #221333;
+    background-color: var(--darkBlue);
+    border: 3px solid var(--darkBlue);
     color: #df3311;
   }
   .login-btn-bg {
@@ -181,35 +148,12 @@ const Wrapper = styled.main`
     border-radius: 1px;
   }
   /* ------------------- */
-  /* Error_statement */
-  /* ------------------- */
-  .alert {
-    border: none;
-    border-radius: 2px;
-    padding: 10px 30px;
-    color: #df3311;
-    font-family: sans-serif;
-    font-size: 20px;
-    font-weight: 200;
-  }
-
-  /* ------------------- */
   /* Media Queries */
   /* ------------------- */
   @media screen and (max-width: 850px) {
-    .login {
-      /* margin-top: 60px; */
-      /* height: 58vh; */
-    }
     .login-btn-block {
       width: 130px;
       height: 41px;
-    }
-    .subscribed-btn-block {
-      height: 41px;
-    }
-    .thank-message h3 {
-      font-size: 22px;
     }
   }
 `;
