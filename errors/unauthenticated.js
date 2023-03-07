fce3e14d1b44e27d21f8bbf7a424fdff44fb2773
SU-T <guitaristpeet@gmail.com>
2023-03-07 15:29:47 -0800

    create UnauthenticatedError

---
+++ errors/unauthenticated.js
@@ -0,0 +1,10 @@
+import CustomError from "./custom-error.js";
+import { StatusCodes } from "http-status-codes";
+
+class UnauthenticatedError extends CustomError {
+  constructor(message) {
+    super(message);
+    this.statusCode = StatusCodes.UNAUTHORIZED;
+  }
+}
+export default UnauthenticatedError;
