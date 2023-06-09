cb8bc99cc7ff0727c743452e89bdb60c57b5631f
SU-T <guitaristpeet@gmail.com>
2023-06-09 12:41:20 +0700

    route the admin-dashboard and dashboard in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,29 @@
 import { Landing, Shows, About, Contact, Register } from "./pages";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  Profile,
+  AddShow,
+  AllShows,
+  MyTickets,
+  Overview,
+  SharedLayout,
+} from "./pages/dashboard";
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/admin-dashboard" element={<SharedLayout />}>
+          <Route index element={<Overview />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="allshows" element={<AllShows />} />
+          <Route path="addshow" element={<AddShow />} />
+        </Route>
+        <Route path="/dashboard" element={<SharedLayout />}>
+          <Route index element={<Overview />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="mytickets" element={<MyTickets />} />
+        </Route>
         <Route path="/" element={<Landing />} />
         <Route path="/shows" element={<Shows />} />
         <Route path="/about" element={<About />} />
