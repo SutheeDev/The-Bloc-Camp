e8ed00ad828d57e287d7e9baee1123f6c54296f2
SU-T <guitaristpeet@gmail.com>
2023-06-12 13:41:27 +0700

    create initial updateUser function

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -100,6 +100,10 @@ const AppProvider = ({ children }) => {
     removeUserFromLocalStorage();
   };
 
+  const updateUser = (currentUser) => {
+    console.log(currentUser);
+  };
+
   return (
     <AppContext.Provider
       value={{
@@ -110,6 +114,7 @@ const AppProvider = ({ children }) => {
         loginUser,
         toggleSidebar,
         logoutUser,
+        updateUser,
       }}
     >
       {children}
