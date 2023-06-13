6af6c6b76bdcdb66eba141e3b6b53a9cc858d4f5
SU-T <guitaristpeet@gmail.com>
2023-06-13 15:29:52 +0700

    add icon to Message component

--- client/src/components/Message.js
+++ client/src/components/Message.js
@@ -1,9 +1,16 @@
 import { useAppContext } from "../context/appContext";
+import { FiX } from "react-icons/fi";
+import { BiCheck } from "react-icons/bi";
 
 const Message = () => {
   const { messageText, messageType } = useAppContext();
   return (
-    <div className={`message message-${messageType}`}> {messageText} </div>
+    <div className={`message message-${messageType}`}>
+      <div className="message-icon">
+        {messageType === "error" ? <FiX /> : <BiCheck />}
+      </div>
+      {messageText}
+    </div>
   );
 };
 export default Message;
