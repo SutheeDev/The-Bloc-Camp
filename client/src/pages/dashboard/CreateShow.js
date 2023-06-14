35d1bb6583758d3d602bb8bfe7a6bdc975841d2b
SU-T <guitaristpeet@gmail.com>
2023-06-14 16:15:27 +0700

    initial setup for CreateShow page

--- client/src/pages/dashboard/CreateShow.js
+++ client/src/pages/dashboard/CreateShow.js
@@ -1,9 +1,30 @@
 import styled from "styled-components";
+import { useAppContext } from "../../context/appContext";
+import { FormRow, Message } from "../../components";
 
 const CreateShow = () => {
+  const { handleInputChange, showMessage } = useAppContext();
+
+  const handleInput = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    handleInputChange({ name, value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Create Show");
+  };
+
   return (
     <Wrapper>
-      <h2>Create Shows</h2>
+      <form className="form" onSubmit={handleSubmit}>
+        <h3 className="form-title">create show</h3>
+        {showMessage && <Message />}
+        <div className="form-content">
+          <div className="form-row-content"></div>
+        </div>
+      </form>
     </Wrapper>
   );
 };
