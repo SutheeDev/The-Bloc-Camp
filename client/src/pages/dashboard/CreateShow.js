dede4cfc849ea834fa8eabf92fed272676baeb21
SU-T <guitaristpeet@gmail.com>
2023-06-14 17:42:46 +0700

    add some input fields to CreateShow

--- client/src/pages/dashboard/CreateShow.js
+++ client/src/pages/dashboard/CreateShow.js
@@ -22,7 +22,28 @@ const CreateShow = () => {
         <h3 className="form-title">create show</h3>
         {showMessage && <Message />}
         <div className="form-content">
-          <div className="form-row-content"></div>
+          <div className="form-row-content">
+            <FormRow type="text" name="artist" />
+            <FormRow type="date" name="date" />
+            <FormRow type="time" name="performTime" labelText="start time" />
+            <FormRow type="text" name="smallImg" labelText="thumbnail image" />
+            <FormRow type="text" name="bigImg" labelText="featured image" />
+            <FormRow
+              type="number"
+              name="ticketPrice"
+              labelText="ticket price"
+            />
+            <FormRow
+              type="checkbox"
+              name="publish"
+              labelText="publish on site?"
+            />
+            <FormRow
+              type="checkbox"
+              name="featured"
+              labelText="featured show?"
+            />
+          </div>
         </div>
       </form>
     </Wrapper>
