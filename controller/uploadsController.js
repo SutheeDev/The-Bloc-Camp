759bbb11eee9e71157c60292472ae06456f5cb91
SU-T <guitaristpeet@gmail.com>
2023-06-16 15:29:01 +0700

    install cloudinary

--- controller/uploadsController.js
+++ controller/uploadsController.js
@@ -1,4 +1,8 @@
+import path from "path";
+import { StatusCodes } from "http-status-codes";
+
 const uploadArtistImage = async (req, res) => {
+  const artistImage = req.files.artistImage;
   res.send("upload artist image");
 };
 
