3bc1c92fc690d1eeaa2023e35a10674f57d32567
SU-T <guitaristpeet@gmail.com>
2023-06-25 09:53:24 +0700

    import and render SearchContainer and EventsContainer on AllShows page

--- client/src/pages/dashboard/AllShows.js
+++ client/src/pages/dashboard/AllShows.js
@@ -1,9 +1,12 @@
 import styled from "styled-components";
+import { SearchContainer, EventsContainer } from "../../components";
 
 const AllShows = () => {
   return (
     <Wrapper>
       <h2>All Shows</h2>
+      <SearchContainer />
+      <EventsContainer />
     </Wrapper>
   );
 };
