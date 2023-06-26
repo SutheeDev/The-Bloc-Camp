8afe9f4f22ba7998bf2f29ed608e6cb3f500c457
SU-T <guitaristpeet@gmail.com>
2023-06-26 15:35:20 +0700

    initial return in EventsContainer
    invoke getShows function with useEffect / check if page isLoading / check if no shows / return Event component

--- client/src/components/EventsContainer.js
+++ client/src/components/EventsContainer.js
@@ -1,11 +1,40 @@
 import Loading from "./Loading";
+import { useAppContext } from "../context/appContext";
+import { useEffect } from "react";
+import styled from "styled-components";
+import Event from "./Event";
 
 const EventsContainer = () => {
+  const { getShows, isLoading, shows, totalShows, numOfPages, page } =
+    useAppContext();
+
+  useEffect(() => {
+    getShows();
+  }, []);
+
+  if (isLoading) {
+    return <Loading center />;
+  }
+
+  if (totalShows.length === 0) {
+    return (
+      <Wrapper>
+        <h1>No upcoming show to display </h1>
+      </Wrapper>
+    );
+  }
+
   return (
-    <main>
-      <Loading />
-      <h1>Events Container</h1>
-    </main>
+    <Wrapper>
+      <h1>
+        {totalShows} upcoming show{totalShows > 1 && "s"} found
+      </h1>
+      {shows.map((show) => {
+        return <Event />;
+      })}
+    </Wrapper>
   );
 };
 export default EventsContainer;
+
+const Wrapper = styled.main``;
