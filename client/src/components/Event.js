533d0c7aef99718da0a7efb6e233fdd9e9ed54c6
SU-T <guitaristpeet@gmail.com>
2023-06-27 19:55:05 +0700

    return date and time in the Event

--- client/src/components/Event.js
+++ client/src/components/Event.js
@@ -1,10 +1,34 @@
-const Event = ({ artist, performDateTime, artistImage }) => {
+import styled from "styled-components";
+import moment from "moment";
+
+const Event = ({ artist, performDateTime, featureImage }) => {
+  let date = moment(performDateTime);
+  let time = moment(performDateTime);
+
+  date = date.format("ddd, MMM Do YYYY");
+  time = time.format("hh:mm a");
+
   return (
-    <div>
-      <h3>{artist}</h3>
-      <h3>{performDateTime}</h3>
-      <image>{artistImage}</image>
-    </div>
+    <Wrapper>
+      <div>
+        <img src={featureImage}></img>
+        <div className="event-date">
+          <h3>{date}</h3>
+          <h3>{time}</h3>
+        </div>
+        <h3>{artist}</h3>
+        <div className="btn-container">
+          <button type="button" className="btn">
+            edit
+          </button>
+          <button type="button" className="btn">
+            delete
+          </button>
+        </div>
+      </div>
+    </Wrapper>
   );
 };
 export default Event;
+
+const Wrapper = styled.div``;
