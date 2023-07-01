4612906cf4caedacd0046797d36e4fcf3027f812
SU-T <guitaristpeet@gmail.com>
2023-07-01 14:25:42 +0700

    change .login padding on Register page

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -175,7 +175,7 @@ const Wrapper = styled.main`
   /* ------------------- */
   background-color: var(--darkBlue);
   .login {
-    padding: 30px 0 30px 0;
+    padding: 20px 0 20px 0;
   }
   .login-container {
     width: 90vw;
