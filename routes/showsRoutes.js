4ec94ff20836ce925278ba8ce0c3eb5394d94f99
SU-T <guitaristpeet@gmail.com>
2023-07-13 10:57:44 +0700

    add showOverview route in the showsRoutes

--- routes/showsRoutes.js
+++ routes/showsRoutes.js
@@ -6,6 +6,7 @@ import {
   getAllShows,
   updateShow,
   deleteShow,
+  showOverview,
 } from "../controller/showsController.js";
 import {
   uploadArtistImage,
@@ -13,6 +14,7 @@ import {
 } from "../controller/uploadsController.js";
 
 router.route("/").post(createShow).get(getAllShows);
+router.route("/overview").get(showOverview);
 router.route("/uploads-artistImage").post(uploadArtistImage);
 router.route("/uploads-featureImage").post(uploadFeatureImage);
 router.route("/:id").patch(updateShow).delete(deleteShow);
