82bf8f7b9860a75c47350b523c6a31b1b94b58fe
SU-T <guitaristpeet@gmail.com>
2023-08-01 14:14:37 +0700

    check isLoading in handleSearch

--- client/src/components/SearchContainer.js
+++ client/src/components/SearchContainer.js
@@ -15,6 +15,7 @@ const SearchContainer = () => {
   } = useAppContext();
 
   const handleSearch = (e) => {
+    if (isLoading) return;
     handleInputChange({ name: e.target.name, value: e.target.value });
   };
 
