e13b92eda6f72ed328afe3bb0c9f5e7109b9a413
SU-T <guitaristpeet@gmail.com>
2023-08-02 16:27:52 +0700

    tried to restyled the Events.js

--- client/src/components/Events.js
+++ client/src/components/Events.js
@@ -53,40 +53,149 @@ const Wrapper = styled.section`
   /* Shows Section */
   /* ----------------- */
   background-color: var(--lighterBlue);
-  padding: 80px 0;
+  padding: 50px 0;
 
   .shows-container {
-    width: 70vw;
+    width: 85vw;
     max-width: 1000px;
     margin: 0 auto;
   }
   .show {
-    padding: 80px 0;
-    border-bottom: 1px solid #ffffff;
     display: flex;
-    justify-content: space-between;
+    flex-direction: column;
+    padding: 50px 0;
+    /* justify-content: space-between; */
     align-items: center;
     overflow: hidden;
+    border-bottom: 1px solid #ffffff;
   }
   .show-info {
+    /* background-color: red; */
+    width: 100%;
     display: flex;
-    justify-content: space-between;
+    justify-content: flex-start;
     align-items: center;
-    gap: 15px;
+    /* gap: 15px; */
     cursor: pointer;
     overflow: hidden;
+    margin-bottom: 20px;
 
     transition: all 0.3s ease-out;
   }
+  .show-info h2 {
+    /* font-size: 30px; */
+    font-size: 1.9rem;
+    /* width: 10%; */
+    width: 45px;
+    /* background-color: wheat; */
+  }
+  .show-info p {
+    font-size: 12px;
+    text-transform: capitalize;
+    /* background-color: black; */
+  }
+  .show-info div {
+    width: 50px;
+  }
+  .show-info h3 {
+    /* background-color: blue; */
+    /* display: block; */
+    /* width: 100%; */
+    /* font-size: 26px; */
+    font-size: 1.65rem;
+    /* padding-left: 15px; */
+    text-transform: capitalize;
+  }
+  .day {
+    color: #726185;
+  }
+  .fancy-btn {
+    position: relative;
+    height: 55px;
+    margin-top: 80px;
+  }
+  .fancy-btn button {
+    text-transform: uppercase;
+    padding: 1rem 1.5rem;
+    color: #221333;
+    border: 3px solid #221333;
+    background-color: transparent;
+    border-radius: 1px;
+    position: absolute;
+    top: 0;
+    left: 50%;
+    transform: translate(-50%, 0);
+    z-index: 2;
+    cursor: pointer;
+    letter-spacing: 1px;
+    font-weight: 700;
+
+    transition: all 0.3s ease;
+  }
+  .fancy-btn div.btn-block {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-73%, -55%);
+    width: 40vh;
+    max-width: 220px;
+    height: 27px;
+    background-color: #ffffff;
+    border-radius: 1px;
+    z-index: 1;
+  }
+  .fancy-btn button:hover {
+    background-color: #221333;
+    color: #df3311;
+  }
+  .btn-bg {
+    width: 230px;
+    height: 74px;
+    background-color: #df3311;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-38%, -50%);
+    border-radius: 1px;
+  }
+  @media screen and (min-width: 450px) {
+    .show {
+      flex-direction: row;
+      justify-content: space-between;
+    }
+    .show-info {
+      width: 65%;
+      margin-bottom: 0;
+    }
+  }
+
+  @media screen and (min-width: 850px) {
+    padding: 80px 0;
+    .shows-container {
+      width: 70vw;
+    }
+    .show {
+      padding: 80px 0;
+    }
+    .show-info h2 {
+      /* font-size: 42px; */
+      font-size: 2.6rem;
+      width: 65px;
+    }
+    .show-info div {
+      width: 65px;
+    }
+    .show-info h3 {
+      font-size: 38px;
+    }
+  }
+  /* 
+  
   .show:hover .show-info {
     transform: translateX(-110px);
   }
-  .show-info h2 {
-    font-size: 42px;
-  }
-  .show-info p {
-    text-transform: capitalize;
-  }
+  
+  
   .show-info h3 {
     font-size: 38px;
     text-transform: capitalize;
@@ -143,25 +252,13 @@ const Wrapper = styled.section`
     left: 50%;
     transform: translate(-38%, -50%);
     border-radius: 1px;
-  }
+  } */
   /* ----------------- */
   /* Shows Section Media Queries */
   /* ----------------- */
-  @media screen and (max-width: 850px) {
-    padding: 50px 0;
+  /* @media screen and (max-width: 850px) {
 
-    .shows-container {
-      width: 85vw;
-    }
-    .show {
-      padding: 50px 0;
-    }
-    .show-info h2 {
-      font-size: 30px;
-    }
-    .show-info p {
-      font-size: 12px;
-    }
+    
     .show-info h3 {
       font-size: 26px;
       padding-left: 15px;
@@ -171,9 +268,7 @@ const Wrapper = styled.section`
     }
   }
   @media screen and (max-width: 450px) {
-    .show {
-      flex-direction: column;
-    }
+    
     .show-info {
       width: 100%;
       justify-content: flex-start;
@@ -185,6 +280,6 @@ const Wrapper = styled.section`
     }
     .show:hover .show-info {
       transform: translateX(-90px);
-    }
-  }
+    } */
+  /* }  */
 `;
