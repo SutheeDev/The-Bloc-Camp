c362e81be873591a1cbbd99b2598969afe7bbc74
SU-T <guitaristpeet@gmail.com>
2023-08-02 10:45:31 +0700

    added skip variable and chanin .skip and .limit with the result

--- controller/showsController.js
+++ controller/showsController.js
@@ -53,10 +53,8 @@ const getAllShows = async (req, res) => {
 
   const page = Number(req.query.page) || 1;
   const limit = Number(req.query.limit) || 10;
-
-  // const skip = 1;
-  // result = result.skip(skip).limit(limit);
-  result = result.limit(limit);
+  const skip = (page - 1) * limit;
+  result = result.skip(skip).limit(limit);
 
   const shows = await result;
   const totalShows = await Show.countDocuments(queryObject);
