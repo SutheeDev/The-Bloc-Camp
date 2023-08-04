71005886ae45a3fe40faec0827e1876860677f4d
SU-T <guitaristpeet@gmail.com>
2023-08-04 10:11:10 +0700

    changed default limit to 12 shows

--- controller/showsController.js
+++ controller/showsController.js
@@ -137,7 +137,7 @@ const getPublishedShows = async (req, res) => {
   let result = Show.find({ isPublished: true }).sort("performDateTime");
 
   const page = Number(req.query.page) || 1;
-  const limit = Number(req.query.limit) || 10;
+  const limit = Number(req.query.limit) || 12;
   const skip = (page - 1) * limit;
   result = result.skip(skip).limit(limit);
 
