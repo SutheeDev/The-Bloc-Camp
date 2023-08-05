c1f47d4770dda63d6b9f35907d0735889428d01b
SU-T <guitaristpeet@gmail.com>
2023-08-05 14:40:29 +0700

    created BarChart and AreaChart components / initially setup ChartsContainer

--- client/src/components/ChartsContainer.js
+++ client/src/components/ChartsContainer.js
@@ -1,4 +1,18 @@
+import BarChart from "./BarChart";
+import AreaChart from "./AreaChart";
+import { useAppContext } from "../context/appContext";
+import { useState } from "react";
+import styled from "styled-components";
+
 const ChartsContainer = () => {
-  return <div>Charts Container</div>;
+  const { monthlyApplication: data } = useAppContext();
+  return (
+    <Wrapper>
+      <BarChart data={data} />
+      <AreaChart data={data} />
+    </Wrapper>
+  );
 };
 export default ChartsContainer;
+
+const Wrapper = styled.div``;
