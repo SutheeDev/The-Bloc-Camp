46a8d6058c6c5267295e148c0f787ca9261726eb
SU-T <guitaristpeet@gmail.com>
2023-08-08 12:17:04 +0700

    fixed path of userLinks in user-links.js

--- client/src/utils/user-links.js
+++ client/src/utils/user-links.js
@@ -4,19 +4,19 @@ const userLinks = [
   {
     id: 0,
     text: "Overview",
-    path: "/admin-dashboard/",
+    path: "/dashboard/",
     icon: <BiBarChartSquare />,
   },
   {
     id: 1,
     text: "My Tickets",
-    path: "/admin-dashboard/my-tickets",
+    path: "/dashboard/my-tickets",
     icon: <BiGridSmall />,
   },
   {
     id: 2,
     text: "Profile",
-    path: "/admin-dashboard/profile",
+    path: "/dashboard/profile",
     icon: <BiBookContent />,
   },
 ];
