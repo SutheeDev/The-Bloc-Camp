0192b4765237c550302d78bd453e41fe41ee3dec
SU-T <guitaristpeet@gmail.com>
2023-08-09 15:34:57 +0700

    initial setup for QuickTipsContainer and QuickTipsitem

--- client/src/components/QuickTipsContainer.js
+++ client/src/components/QuickTipsContainer.js
@@ -1,4 +1,36 @@
+import {
+  AiOutlineSnippets,
+  AiOutlineSolution,
+  AiOutlineProfile,
+  AiOutlineLike,
+} from "react-icons/ai";
+import QuickTipsItem from "./QuickTipsItem";
+import styled from "styled-components";
+
 const QuickTipsContainer = () => {
-  return <div>QuickTipsContainer</div>;
+  const defaultQuickTips = [
+    {
+      icon: <AiOutlineProfile />,
+      title: "account check-up",
+      text: "been awhile? Consider updating your profile.",
+      btn: "edit profile",
+    },
+    {
+      icon: <AiOutlineLike />,
+      title: "pick your favorites",
+      text: `Pick your favorites for the best way to know what’s happening live near you.`,
+      btn: "pick favorites",
+    },
+  ];
+
+  return (
+    <Wrapper>
+      {defaultQuickTips.map((item, index) => {
+        <QuickTipsItem key={index} {...item} />;
+      })}
+    </Wrapper>
+  );
 };
 export default QuickTipsContainer;
+
+const Wrapper = styled.div``;
--- client/src/components/QuickTipsItem.js
+++ client/src/components/QuickTipsItem.js
@@ -1,4 +1,18 @@
-const QuickTipsItem = () => {
-  return <div>QuickTipsItem</div>;
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const QuickTipsItem = ({ icon, title, text, btn }) => {
+  return (
+    <Wrapper>
+      <div className="icon">{icon}</div>
+      <div className="tip-info">
+        <h3>{title}</h3>
+        <p>{text}</p>
+        <Link></Link>
+      </div>
+    </Wrapper>
+  );
 };
 export default QuickTipsItem;
+
+const Wrapper = styled.div``;
