4aef7102ab813928c3cd7eee75695dad3cbb9f66
SU-T <guitaristpeet@gmail.com>
2023-08-09 11:07:22 +0700

    prevented log back in automatically
    grabbed the logoutUser from useAppContext and invoked it in ProtectedRoute and UserProtectedRoute, so when the user (admin or user role) visit the unauthorized pages (cross admin or user dashboard), we will logoutUser and remove user and token in the local storage to prevent automatic log back in

--- client/src/pages/ProtectedRoute.js
+++ client/src/pages/ProtectedRoute.js
@@ -2,8 +2,9 @@ import { Navigate } from "react-router-dom";
 import { useAppContext } from "../context/appContext";
 
 const ProtectedRoute = ({ children }) => {
-  const { user } = useAppContext();
+  const { user, logoutUser } = useAppContext();
   if (!user || user.role !== "admin") {
+    logoutUser();
     return <Navigate to="/register" />;
   }
   return children;
--- client/src/pages/UserProtectedRoute.js
+++ client/src/pages/UserProtectedRoute.js
@@ -2,8 +2,9 @@ import { Navigate } from "react-router-dom";
 import { useAppContext } from "../context/appContext";
 
 const UserProtectedRoute = ({ children }) => {
-  const { user } = useAppContext();
+  const { user, logoutUser } = useAppContext();
   if (!user || user.role !== "user") {
+    logoutUser();
     return <Navigate to="/register" />;
   }
   return children;
