98a10e596d9baaf58efdfb1b1d302276c55a4177
SU-T <guitaristpeet@gmail.com>
2023-08-15 12:17:47 +0700

    passed and created getUserFavorites route on the authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,11 +7,13 @@ import {
   login,
   updateUser,
   updateUserFavorites,
+  getUserFavorites,
 } from "../controller/authController.js";
 
 router.route("/register").post(register);
 router.route("/login").post(login);
 router.route("/updateuser").patch(authenticatedUser, updateUser);
+router.route("/favorites").get(authenticatedUser, getUserFavorites);
 router.route("/favorites/:id").patch(authenticatedUser, updateUserFavorites);
 
 export default router;
