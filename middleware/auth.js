142252f8a14bb34d236bfea4a511091cc4e95716
SU-T <guitaristpeet@gmail.com>
2023-08-20 21:04:45 +0700

    created testUser and testAdmin by comparing the userId with the ids and passed them into the req.user

--- middleware/auth.js
+++ middleware/auth.js
@@ -11,7 +11,14 @@ const auth = async (req, res, next) => {
   const token = authHeader.split(" ")[1];
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = { userId: payload.userId, role: payload.role };
+    const testUser = payload.userId === "64e219e1f99df9e1e9cc68bb";
+    const testAdmin = payload.userId === "64e21a91f99df9e1e9cc68d9";
+    req.user = {
+      userId: payload.userId,
+      role: payload.role,
+      testUser,
+      testAdmin,
+    };
     next();
   } catch (error) {
     throw new UnauthenticatedError("Authentication Invalid");
