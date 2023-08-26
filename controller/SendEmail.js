dfba884e193ad870121d8c1a6e226bdfa6de56a8
SU-T <guitaristpeet@gmail.com>
2023-08-26 21:31:54 +0700

    created SendEmail controller

---
+++ controller/SendEmail.js
@@ -0,0 +1,25 @@
+import nodemailer from "nodemailer";
+
+const sendEmail = async (req, res) => {
+  let testAccount = await nodemailer.createTestAccount();
+
+  const transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    auth: {
+      user: "mitchel13@ethereal.email",
+      pass: "4NRdmsXnPfRxTNPr93",
+    },
+  });
+
+  let info = await transporter.sendMail({
+    from: ' "Suthee Developer" <test@test.com>',
+    to: "bar@example.com",
+    subject: "Hello",
+    html: "<h2>Sending Email Subscribtion</h2>",
+  });
+
+  res.json(info);
+};
+
+export default sendEmail;
