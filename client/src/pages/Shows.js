b1a05bf2bbb2c0cad4f5e9006ea6ca647009a36b
SU-T <guitaristpeet@gmail.com>
2023-08-27 13:46:47 +0700

    styled the loadMore-btn

--- client/src/pages/Shows.js
+++ client/src/pages/Shows.js
@@ -65,8 +65,8 @@ const Shows = () => {
         {numOfPages > 1 && <PageBtnContainer />}
       </div> */}
       {visibleItems < shows.length && (
-        <div className="page-btn-container">
-          <button>load more</button>
+        <div className="loadMore-container">
+          <button className="loadMore-btn">load more</button>
         </div>
       )}
 
@@ -86,14 +86,14 @@ const Wrapper = styled.main`
     margin-top: -5px;
   }
   .calendar-container,
-  .page-btn-container {
+  .loadMore-container {
     width: 75vw;
     margin: 0 auto;
     @media screen and (max-width: 850px) {
       width: 80vw;
     }
   }
-  .page-btn-container {
+  .loadMore-container {
     display: flex;
     justify-content: center;
   }
@@ -137,5 +137,31 @@ const Wrapper = styled.main`
   .pageNum-btn.active:hover {
     color: var(--darkBlue);
     border: 3px solid var(--reddish);
+  }
+  .loadMore-btn {
+    width: 100%;
+    text-transform: uppercase;
+    padding: 1rem 1.5rem;
+    color: var(--reddish);
+    border: 3px solid var(--reddish);
+    background-color: transparent;
+    border-radius: 1px;
+    cursor: pointer;
+    letter-spacing: 1px;
+    font-weight: 700;
+    margin: 2em 0 1em 0;
+
+    transition: all 0.3s ease;
+  }
+  .loadMore-btn:hover {
+    color: var(--white);
+    border: 3px solid var(--darkRed);
+  }
+  @media screen and (min-width: 450px) {
+    .loadMore-btn {
+      width: 45%;
+      max-width: 250px;
+      margin: 4em 0 1em 0;
+    }
   }
 `;
