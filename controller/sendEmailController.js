e943178f29f5ef695aead43306fad7838d089169
SU-T <guitaristpeet@gmail.com>
2023-09-09 15:12:50 +0700

    add api_key to .env / create a new sendEmailBrevo controller

--- controller/sendEmailController.js
+++ controller/sendEmailController.js
@@ -2,32 +2,34 @@ import nodemailer from "nodemailer";
 import { StatusCodes } from "http-status-codes";
 import { BadRequestError } from "../errors/index.js";
 
-const sendEmailEthereal = async (req, res) => {
-  const { subscribeEmail } = req.body;
+// const sendEmailEthereal = async (req, res) => {
+//   const { subscribeEmail } = req.body;
 
-  if (!subscribeEmail) {
-    throw new BadRequestError("Please provide your email address");
-  }
+//   if (!subscribeEmail) {
+//     throw new BadRequestError("Please provide your email address");
+//   }
 
-  let testAccount = await nodemailer.createTestAccount();
+//   let testAccount = await nodemailer.createTestAccount();
 
-  const transporter = nodemailer.createTransport({
-    host: "smtp.ethereal.email", // put this in the .env / swap this in the production
-    port: 587,
-    auth: {
-      user: "edwin.balistreri@ethereal.email",
-      pass: "bUWKvuApCex7Vm2S3U",
-    },
-  });
+//   const transporter = nodemailer.createTransport({
+//     host: "smtp.ethereal.email", // put this in the .env / swap this in the production
+//     port: 587,
+//     auth: {
+//       user: "edwin.balistreri@ethereal.email",
+//       pass: "bUWKvuApCex7Vm2S3U",
+//     },
+//   });
 
-  let info = await transporter.sendMail({
-    from: ' "SutheeDeveloper" <test@test.com>',
-    to: subscribeEmail,
-    subject: "Thanks for subscribing!",
-    html: "<h2>Sending Email Subscribtion</h2>",
-  });
+//   let info = await transporter.sendMail({
+//     from: ' "SutheeDeveloper" <test@test.com>',
+//     to: subscribeEmail,
+//     subject: "Thanks for subscribing!",
+//     html: "<h2>Sending Email Subscribtion</h2>",
+//   });
 
-  res.status(StatusCodes.OK).json({ info });
-};
+//   res.status(StatusCodes.OK).json({ info });
+// };
 
-export default sendEmailEthereal;
+const sendEmailBrevo = async (req, res) => {};
+
+export default sendEmailBrevo;
